37da047f1c856eb60fc7a55677c3de21474197f2
Nunes Alfredo Assano <105804634+Tuvea21@users.noreply.github.com>
2025-08-08 09:45:08 +0200

    Update

--- 02_both_ends.py
+++ 02_both_ends.py
@@ -8,13 +8,13 @@ for menor que 2, retorne uma string vazia.
 """
 
 def both_ends(s):
-    # +++ SUA SOLUÇÃO +++
+    # +++ SUA SOLUÇÃO ++
     if len(s) < 2:
         return ''
     return s[:2] + s[-2:]
 
 
-# --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
+# --- Daqui para baixo são apenas códigos auxiliáries de teste. --
 
 def test(f, in_, expected):
     """
